0dcac2c711f5d408c280a79b0a9285647d01234d
Maximilian Ried <79225941+maximilianried@users.noreply.github.com>
2021-10-26 10:10:17 +0200

    Refactoring

--- src/main/java/com/maximilianried/keychainconverter/Application.java
+++ src/main/java/com/maximilianried/keychainconverter/Application.java
@@ -9,9 +9,9 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Hello!");
+        stage.setTitle("Keychain Converter");
         stage.setScene(scene);
         stage.show();
     }
