803b6c6fa88997f68f501d7dfefc8192084a6599
Maximilian Ried <79225941+maximilianried@users.noreply.github.com>
2021-10-26 20:53:23 +0200

    Add File chooser dialog

--- src/main/java/com/maximilianried/keychainconverter/Controller.java
+++ src/main/java/com/maximilianried/keychainconverter/Controller.java
@@ -1,7 +1,15 @@
 package com.maximilianried.keychainconverter;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.File;
 
 public class Controller {
     @FXML
@@ -10,5 +18,16 @@ public class Controller {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void openFile(ActionEvent event) {
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+
+        FileChooser filechooser = new FileChooser();
+        filechooser.setTitle("Select Exported CSV File");
+        File selected = filechooser.showOpenDialog(stage);
+
+        System.out.println(selected);
     }
 }
