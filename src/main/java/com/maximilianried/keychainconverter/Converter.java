30f1f9e49f3df6ae4e18a48ffb65672004bf523a
Maximilian Ried <79225941+maximilianried@users.noreply.github.com>
2021-10-29 23:27:44 +0200

    Algorithm for the conversion

--- src/main/java/com/maximilianried/keychainconverter/Converter.java
+++ src/main/java/com/maximilianried/keychainconverter/Converter.java
@@ -27,47 +27,16 @@ public class Converter {
     public static void bitwarden(CSVReader csvReader) throws CsvValidationException, IOException {
         String[] nextRecord;String home = System.getProperty("user.home");
         String homePath = home + "/converted.csv";
+        CSVWriter csvWriter = new CSVWriter(new FileWriter(homePath));
 
         while ((nextRecord = csvReader.readNext()) != null) {
-            System.out.println("Folder : " + nextRecord[0]);
-            System.out.println("Favorite : " + nextRecord[1]);
-            System.out.println("Type : " + nextRecord[2]);
-            System.out.println("Name : " + nextRecord[3]);
-            System.out.println("Notes : " + nextRecord[4]);
-            System.out.println("Fields : " + nextRecord[5]);
-            System.out.println("Reprompt : " + nextRecord[6]);
-
             if (!nextRecord[7].contains("http") && !nextRecord[7].equals("")) {
                 nextRecord[7] = "https://" + nextRecord[7];
             }
 
-            System.out.println("URL : " + nextRecord[7]);
-            System.out.println("Username : " + nextRecord[8]);
-            System.out.println("Password : " + nextRecord[9]);
-            System.out.println("TOTP : " + nextRecord[10]);
-            System.out.println("==========================");
-
-            /*
-            try (
-                Writer writer = Files.newBufferedWriter(Paths.get(homePath));
-
-                CSVWriter csvWriter = new CSVWriter(writer,
-                        CSVWriter.DEFAULT_SEPARATOR,
-                        CSVWriter.NO_QUOTE_CHARACTER,
-                        CSVWriter.DEFAULT_ESCAPE_CHARACTER,
-                        CSVWriter.DEFAULT_LINE_END);
-            ) {
-                String[] headerRecord = {"Name", "URL", "Password", "TOTP"};
-                csvWriter.writeNext(headerRecord);
-
-                csvWriter.writeNext(new String[]{nextRecord[3]});
-                csvWriter.writeNext(new String[]{nextRecord[7]});
-                csvWriter.writeNext(new String[]{nextRecord[9]});
-                csvWriter.writeNext(new String[]{nextRecord[10]});
-            }
-            */
+            csvWriter.writeNext(new String[]{nextRecord[7], nextRecord[8], nextRecord[9], nextRecord[10]});
         }
 
-
+        csvWriter.close();
     }
 }
