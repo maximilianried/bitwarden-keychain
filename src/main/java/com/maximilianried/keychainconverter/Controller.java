bdf356b0cc3f8138ca1ae2ea8b693db2c996bfe7
Maximilian Ried <79225941+maximilianried@users.noreply.github.com>
2021-10-30 01:19:33 +0200

    Change on conversion algorithm

--- src/main/java/com/maximilianried/keychainconverter/Controller.java
+++ src/main/java/com/maximilianried/keychainconverter/Controller.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -14,7 +15,7 @@ import java.io.IOException;
 
 public class Controller {
     @FXML
-    protected void openFile(ActionEvent event) throws IOException {
+    protected void openFile(ActionEvent event) {
         Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
 
         FileChooser filechooser = new FileChooser();
@@ -23,7 +24,20 @@ public class Controller {
                 new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
 
         filechooser.setTitle("Exportierte CSV Datei auswählen");
-        File selected = filechooser.showOpenDialog(stage);
-        Converter.getFile(selected);
+
+        try {
+            File selected = filechooser.showOpenDialog(stage);
+            Converter.getFile(selected);
+        } catch (IOException e) {
+            selectError();
+        }
+    }
+
+    public static void selectError() {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Keychain Converter");
+        alert.setHeaderText("Datei nicht gefunden");
+        alert.setContentText("Die Datei konnte nicht gefunden werden, bittw wähle eine Datei!");
+        alert.showAndWait();
     }
 }
--- src/main/java/com/maximilianried/keychainconverter/Converter.java
+++ src/main/java/com/maximilianried/keychainconverter/Converter.java
@@ -3,40 +3,65 @@ package com.maximilianried.keychainconverter;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 import com.opencsv.exceptions.CsvValidationException;
+import javafx.scene.control.Alert;
 
 
 public class Converter {
 
     public static void getFile(File selected) throws IOException {
-        try (
+        try {
             Reader reader = Files.newBufferedReader(Path.of(String.valueOf(selected)));
             CSVReader csvReader = new CSVReader(reader);
-        ) {
             bitwarden(csvReader);
         } catch (CsvValidationException e) {
-            System.out.println("Failed read file");
             e.printStackTrace();
         }
     }
 
     public static void bitwarden(CSVReader csvReader) throws CsvValidationException, IOException {
-        String[] nextRecord;String home = System.getProperty("user.home");
-        String homePath = home + "/converted-for-keychain.csv";
+        String[] nextRecord;
+        String home = System.getProperty("user.home") + "/converted-for-keychain.csv";
 
-        CSVWriter csvWriter = new CSVWriter(new FileWriter(homePath));
+        CSVWriter csvWriter = new CSVWriter(new FileWriter(home));
         csvWriter.writeNext(new String[]{"Title", "Url", "Username", "Password", "OTPAuth"});
 
-        while ((nextRecord = csvReader.readNext()) != null) {
-            if (!nextRecord[7].contains("http") && !nextRecord[7].equals("")) {
-                nextRecord[7] = "https://" + nextRecord[7];
+        // TODO delete first line of imported file
+
+        try {
+            while ((nextRecord = csvReader.readNext()) != null) {
+                if (!nextRecord[7].contains("http") && !nextRecord[7].equals("")) {
+                    nextRecord[7] = "https://" + nextRecord[7];
+                }
+                csvWriter.writeNext(new String[]{nextRecord[3], nextRecord[7], nextRecord[8], nextRecord[9], nextRecord[10]});
             }
 
-            csvWriter.writeNext(new String[]{nextRecord[3], nextRecord[7], nextRecord[8], nextRecord[9], nextRecord[10]});
-        }
+            csvWriter.close();
+            exportSuccess();
 
-        csvWriter.close();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            Files.delete(Paths.get(home));
+            fileError();
+        }
+    }
+
+    public static void exportSuccess() {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Keychain Converter");
+        alert.setHeaderText("Erfolgreich abgeschlossen");
+        alert.setContentText("Vergiss nicht die Datei nach dem erfolgreichem Import zu löschen da diese nicht verschlüsselt ist!");
+        alert.showAndWait();
+    }
+
+    public static void fileError() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Keychain Converter");
+        alert.setHeaderText("Datei konnte nicht gelesen werden");
+        alert.setContentText("Überprüfe bitte, ob der richtige Passwort-Manager ausgewählt ist von dem die Datei stammt.");
+        alert.showAndWait();
     }
 }
