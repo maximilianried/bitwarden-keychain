f3e9defdd51eb3e972dd98c912c56b49bef886da
Maximilian Ried <79225941+maximilianried@users.noreply.github.com>
2021-11-01 22:04:55 +0100

    Formatting changes

--- src/main/java/com/maximilianried/keychainconverter/Application.java
+++ src/main/java/com/maximilianried/keychainconverter/Application.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
