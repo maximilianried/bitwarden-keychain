35c0d5f1c4fdb650bf4f09384b5e93d8ec1fc8b1
Maximilian Ried <79225941+maximilianried@users.noreply.github.com>
2021-11-01 22:05:44 +0100

    Removing old dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,6 @@
 module com.maximilianried.keychainconverter {
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
     requires opencsv;
 
     opens com.maximilianried.keychainconverter to javafx.fxml;
